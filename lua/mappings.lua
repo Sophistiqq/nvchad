88e24918123c0e6d50434c83ae9c9bd0f3f785f5
Roi Borromeo <roi.for.school@gmail.com>
2024-06-12 13:41:48 +0000

    Made the switch window keys into arrow keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("i","<CR>", "<CR>", {desc = "Insert newline"})
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-Left>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-Right>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-Down>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-Up>", "<C-w>k", { desc = "switch window up" })
+
